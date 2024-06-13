f0e75cc6c7c10dec10879fb27811ff3ef23e12e1
alamin6688 <alamin.kzs06@gmail.com>
2024-06-13 22:27:54 +0600

    Payment page modified

--- src/Pages/Payments/Payments.jsx
+++ src/Pages/Payments/Payments.jsx
@@ -2,8 +2,11 @@
 
 const Payments = () => {
     return (
-        <div>
-            <h2>payment</h2>
+        <div className="min-h-screen bg-[#1F2937] mt-6 mb-16 pt-8 rounded-xl">
+            <h2 className="text-4xl text-white font-extrabold text-center">Payment</h2>
+            <p className="w-full text-[16px] text-gray-200 md:w-3/4 mx-auto text-center mt-4">
+              Make your payment to be our Pro-Member/Pro-User. You will get some extra benifits. Stay connected with usfor more offers
+            </p>
         </div>
     );
 };
